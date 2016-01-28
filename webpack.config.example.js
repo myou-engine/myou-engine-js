86d4f961e041800e1192ab5bf7ca2cd468c7d802
Julio Manuel López Tercero <moondos@gmail.com>
2016-01-28 19:32:57 +0100

    Added banner plugin on webpack.config files.

--- webpack.config.example.js
+++ webpack.config.example.js
@@ -22,7 +22,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
-                loader: "url-loader?limit=18000&name=[path][name].[ext]",
+                loader: 'url-loader?limit=18000&name=[path][name].[ext]',
             },
             {test: /\.svg$/, loader: 'url-loader?mimetype=image/svg+xml'},
             {test: /\.woff2?$/, loader: 'url-loader?mimetype=application/font-woff'},
@@ -33,12 +33,42 @@ module.exports = {
     },
     output: {
         path: __dirname + '/example/build/',
-        filename: "myou.js",
+        filename: 'myou.js',
     },
     devtool: 'inline-source-map',
     plugins: [
+        new webpack.BannerPlugin([
+            '"use strict";',
+            '/**',
+            ' * Myou Engine',
+            ' *',
+            ' * Copyright (c) 2016 by Alberto Torres Ruiz <kungfoobar@gmail.com>',
+            ' * Copyright (c) 2016 by Julio Manuel López Tercero <julio@pixelements.net>',
+            ' *',
+            ' *',
+            ' * Permission is hereby granted, free of charge, to any person obtaining a copy',
+            ' * of this software and associated documentation files (the "Software"), to deal',
+            ' * in the Software without restriction, including without limitation the rights',
+            ' * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell',
+            ' * copies of the Software, and to permit persons to whom the Software is',
+            ' * furnished to do so, subject to the following conditions:',
+            ' *',
+            ' * The above copyright notice and this permission notice shall be included in',
+            ' * all copies or substantial portions of the Software.',
+            ' *',
+            ' * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR',
+            ' * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,',
+            ' * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE',
+            ' * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER',
+            ' * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,',
+            ' * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE',
+            ' * SOFTWARE.',
+            ' */'
+        ].join('\n'), {
+            raw: true
+        }),
     ],
     resolve: {
-        extensions: ["", ".webpack.js", ".web.js", ".js", ".coffee", ".json"]
+        extensions: ['', '.webpack.js', '.web.js', '.js', '.coffee', '.json']
     },
 }
